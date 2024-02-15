1d522ebc512f7b8420feb35f6815e13fab268873
evastepanek <estepanek@student.tgm.ac.at>
2024-02-15 12:09:07 +0100

    used generics and tested class Stack

--- src/estepanek/Stack.java
+++ src/estepanek/Stack.java
@@ -7,21 +7,51 @@ package estepanek;
  */
 public class Stack <T>{
     private Object[] array;
+    private int size;
+    private int counter = 0;
     public Stack(){
         this.array = new Object[10];
+        this.size = 10;
     }
-    public Stack(Object[] array){
-        this.array = array;
+    public Stack(int size){
+       this.size = size;
+       this.array = new Object[size];
     }
-    public void push(T add){
-        //neues hinzufuegen
+    public void push(T add) throws StackFullException{
+        if(counter >= size){
+            throw new StackFullException("Stack is full. Please create a new one!");
+        }
+        this.array[counter] = add;
+        counter++;
+
     }
-    public T pop(){
-        //letzes element geloescht
-        return null;
+    public T pop() throws StackEmptyException{
+        if(counter == 0){
+            throw new StackEmptyException("Stack is empty therefore you can not delete an item");
+        }
+        T item = (T) this.array[counter-1];
+        this.array[counter-1] = null;
+        counter--;
+        return item;
     }
-    public T peek(){
-        //value letztes element
-        return null;
+    public T peek() throws StackEmptyException{
+        if(counter == 0){
+            throw new StackEmptyException("Stack is empty therefore you can not read any value of an item");
+        }
+        return (T) this.array[counter-1];
+    }
+    public String list(){
+        String text = "";
+        for(int i = 0; i <= counter; i++){
+            if(this.array[i] != null){
+                if(i == 0){
+                    text += this.array[i].toString();
+                }
+                else {
+                    text += "; " + this.array[i].toString();
+                }
+            }
+        }
+        return text;
     }
 }
--- src/estepanek/Test.java
+++ src/estepanek/Test.java
@@ -7,6 +7,42 @@ package estepanek;
  */
 public class Test {
     public static void main(String[] args){
+        //String array
+        Stack <String> stack1 = new Stack<>();
+        Stack <Integer> stack2 = new Stack<>(4);
+        System.out.println("String array");
+        stack1.push("String");
+        System.out.println("list: " + stack1.list());
+        System.out.println(stack1.pop());
+        System.out.println("list: " + stack1.list());
+        try{
+            System.out.println(stack1.peek());
+        }catch(StackEmptyException see1){
+            System.err.println(see1.getMessage());
+        }
+        System.out.println(stack1.list());
+        System.out.println("");
+        System.out.println("int array");
+        //int array
+        stack2.push(6);
+        System.out.println("list: " + stack2.list());
+        System.out.println(stack2.pop());
+        System.out.println("list: " + stack2.list());
+        try{
+            System.out.println(stack2.peek());
+        }catch(StackEmptyException see2){
+            System.err.println(see2.getMessage());
+        }
+        System.out.println("list: " + stack2.list());
+        stack2.push(6);
+        stack2.push(33);
+        stack2.push(1);
+        stack2.push(8);
+        try {
+            stack2.push(12);
+        }catch(StackFullException sfe){
+            System.err.println(sfe.getMessage());
+        }
 
     }
 }
