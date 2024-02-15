76facbc109e5d450b0dc916c1893af21d4c0cd54
evastepanek <estepanek@student.tgm.ac.at>
2024-02-15 12:53:39 +0100

    comments

--- src/estepanek/Stack.java
+++ src/estepanek/Stack.java
@@ -9,14 +9,29 @@ public class Stack <T>{
     private Object[] array;
     private int size;
     private int counter = 0;
+
+    /**
+     * Contructor with an array length of 10
+     */
     public Stack(){
         this.array = new Object[10];
         this.size = 10;
     }
+
+    /**
+     * Contructor with a variable array size
+     * @param size
+     */
     public Stack(int size){
        this.size = size;
        this.array = new Object[size];
     }
+
+    /**
+     * method for adding an item in the array
+     * @param add iten to add
+     * @throws StackFullException is thrown when the stack is already full
+     */
     public void push(T add) throws StackFullException{
         if(counter >= size){
             throw new StackFullException("Stack is full. Please create a new one!");
@@ -25,6 +40,12 @@ public class Stack <T>{
         counter++;
 
     }
+
+    /**
+     * method for deleting the last added item to the stack
+     * @return the value of the deleted item
+     * @throws StackEmptyException is thrown when the stack is empty so therfore there is nothing to delete
+     */
     public T pop() throws StackEmptyException{
         if(counter == 0){
             throw new StackEmptyException("Stack is empty therefore you can not delete an item");
@@ -34,12 +55,23 @@ public class Stack <T>{
         counter--;
         return item;
     }
+
+    /**
+     * method for returning the value of the last added item of the stack
+     * @return the value
+     * @throws StackEmptyException is throws when the stack is empty therefore there is nothing to return
+     */
     public T peek() throws StackEmptyException{
         if(counter == 0){
             throw new StackEmptyException("Stack is empty therefore you can not read any value of an item");
         }
         return (T) this.array[counter-1];
     }
+
+    /**
+     * listing all the items in the array
+     * @return the listing as a string
+     */
     public String list(){
         String text = "";
         for(int i = 0; i <= counter; i++){
--- src/estepanek/Test.java
+++ src/estepanek/Test.java
@@ -43,6 +43,5 @@ public class Test {
         }catch(StackFullException sfe){
             System.err.println(sfe.getMessage());
         }
-
     }
 }
